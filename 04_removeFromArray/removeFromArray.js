7a7c2a9d35b5a5c4a84c74dfd603157a0915a672
David Pedroza <pedrozadavid68@gmail.com>
2022-10-31 22:02:28 -0700

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...params) {
+    let paramsToRemove = params;
+    let indexArr = [];
 
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < paramsToRemove.length;j++){
+            if(paramsToRemove[j] === arr[i]){
+                indexArr.push(i);
+            }
+        }
+    }
+
+    indexArr = indexArr.sort();
+    //console.log(indexArr);
+
+    let count = indexArr.length;
+    while(count > 0){
+        //console.log(arr);
+        arr.splice(indexArr[0],1);
+        indexArr.shift();
+        indexArr = indexArr.map(num => num-1);
+        count--;
+    }
+    //console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
