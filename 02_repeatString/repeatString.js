4ea729bc489bb1b16ce92f928fddf4597dd17490
David Pedroza <pedrozadavid68@gmail.com>
2022-10-31 20:10:43 -0700

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(repStr, count) {
+    if (count == 0){
+        return '';
+    }
+    if (count < 0){
+        return 'ERROR';
+    }
+    retString = '';
+    while(count > 0){
+        retString += repStr;
+        count--;
+    }
+    return retString;
 };
 
 // Do not edit below this line
