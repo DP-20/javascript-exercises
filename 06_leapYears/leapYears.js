3a1a071a0324b471d50862e2b482c519b2ca6452
David Pedroza <pedrozadavid68@gmail.com>
2022-11-07 19:23:32 -0800

    Finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
+    //check if the leap Year is divisible by 100 and 400
+    if(leapYear % 100 == 0 && leapYear % 400 == 0){
+        return true;
+    }
 
+    //check if leap year is divisible by 4 and not 100
+    if(leapYear % 4 == 0 && leapYear % 100 != 0){
+        return true;
+    }
+
+    // if neither of the above are true then just return false
+    return false;
 };
 
 // Do not edit below this line
