e4c604ecc0f2bd3d73c1974c8670241ff980ff0e
David Pedroza <pedrozadavid68@gmail.com>
2022-11-07 19:05:02 -0800

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if(numA < 0 || numB < 0){
+        return "ERROR";
+    }
 
+    if(typeof(numA) != "number" || typeof(numB) != "number"){
+        return "ERROR"
+    }
+    let sum = 0;
+    let max = 0;
+    let min = 0;
+
+    // get max Number
+    if(numB > numA){
+        max = numB;
+        min = numA;
+    }
+    else{
+        max = numA;
+        min = numB;
+
+    }
+
+    while(max > min-1){
+        sum = sum + max;
+        max--;
+    }
+    return sum;
 };
 
 // Do not edit below this line
